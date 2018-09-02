b9dd2f1bfed2e6d51a709fe78c65a65cf37b0744
Arthur Allshire <arthur@allshire.org>
2018-09-02 15:01:00 +1000

    Initial commit of swerve files

---
+++ tests/pyfrc_test.py
@@ -0,0 +1,8 @@
+'''
+    This test module imports tests that come with pyfrc, and can be used
+    to test basic functionality of just about any robot.
+'''
+
+# We deliberately avoid importing test_autonomous here,
+# because we already test all our autonomous modes (see auto_test.py).
+from pyfrc.tests import test_disabled, test_operator_control, test_practice, test_fuzz
